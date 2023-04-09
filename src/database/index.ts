99d888f8f8b912bfb1f518fa0050402b8a5fb73c
Dominik Bühler <mortelligaming@github.com>
2023-04-09 17:57:12 +0200

    smoother imports

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,10 +2,16 @@ import { Sequelize } from 'sequelize-typescript';
 import { LoginToken } from './loginToken';
 import { RefreshToken } from './refreshToken';
 import { User, UserRole } from './user';
+import { type TokenContent, type TokenUser, TokenType } from '../middleware/types';
 
 export {
+  type TokenContent,
+  type TokenUser,
   User,
-  UserRole
+  UserRole,
+  LoginToken, 
+  RefreshToken,
+  TokenType
 }
 
 export const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, { 
